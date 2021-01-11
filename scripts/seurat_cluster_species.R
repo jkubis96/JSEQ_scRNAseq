4609c15829c2bd45cf32b43a73072e82121bf47e
Qubix96 <jakub.kubis1996@gmail.com>
2021-01-11 09:10:47 +0100

    Improved clustering

--- scripts/seurat_cluster_species.R
+++ scripts/seurat_cluster_species.R
@@ -2,11 +2,11 @@ library(Seurat)
 library(patchwork)
 library(tidyverse)
 
-
+options(scipen = 999)
+memory.limit(30000000)
+memory.size(T)
 
 args <- commandArgs()
-
-species <- FALSE
 
 
 #Paths and arguments from env
@@ -26,11 +26,8 @@ source(functions)
 
 #########################################################
 
-if (species == 'human') {
-  markers <- readxl::read_xlsx(markers, sheet = 1)
-} else if (species == 'mice') {
-  markers <- readxl::read_xlsx(markers, sheet = 2)
-}
+  markers_class <- readxl::read_xlsx(markers, sheet = 1)
+  markers_subclass <- readxl::read_xlsx(markers, sheet = 2, col_names = F)
 
 ##########################################################
 
@@ -41,12 +38,10 @@ UMI_raw <- Read10X(seurat_umi, gene.column = 1)
 
 #Create SeuratObject
 UMI <- CreateSeuratObject(counts = UMI_raw, project = project_name, min.cells = 1, min.features = 1)
-UMI
+Idents(UMI) <- make.unique(as.character(head(Idents(UMI))))
 }
 
-###########################ZMIENIÆ###############################
-cells_number <- length(UMI@active.ident)
-cells_number <- as.numeric(cells_number)
+
 #################################################################
 
 #Create Ribo and Mito percent stats
@@ -82,8 +77,7 @@ dev.off()
 
 #Droplet content and QC
 
-n_gen <- as.numeric(quantile(UMI@meta.data$nGenes, 0.75))
-
+n_gen <- as.numeric(quantile(sort(UMI@meta.data$nGenes, decreasing = F), 0.99))
 QC_UMI <- data.frame()
 QC_UMI <- as.data.frame(UMI$nGenes)
 QC_UMI$V2 <- UMI$MitoPercent
@@ -138,7 +132,9 @@ rm(QC_UMI)
 
 #Selecting right cells
 
-UMI <- subset(UMI, subset = nGenes > 200 & nGenes < n_gen & MitoPercent < 5)
+UMI <- subset(UMI, subset = nGenes > 200 & nGenes <= n_gen & MitoPercent < 5)
+n_gen <- as.numeric(quantile(sort(UMI@meta.data$nGenes, decreasing = F), 0.95))*0.25
+cells_number <- as.numeric(length(UMI@active.ident))
 
 #####################################################################################
 
@@ -148,21 +144,25 @@ UMI <- NormalizeData(UMI, normalization.method = "LogNormalize", scale.factor = 
 ######################################################################################
 
 #For normalized expression matrix input
-
-if (data == 2) {
-  UMI@assays$RNA@data <- UMI@assays$RNA@counts
+decision <- mean(UMI@assays$RNA@counts@x) < 15
+if (data == 2 & decision == T) {
+  UMI@assays$RNA@data@x <- UMI@assays$RNA@counts@x
+} else if (data == 2 & decision == F) {
+  UMI@assays$RNA@data@x <- log(UMI@assays$RNA@counts@x+1)
 }
 
 UMI <- FindVariableFeatures(UMI, selection.method = "vst", nfeatures = n_gen)
 
 # Identify the 10 most highly variable genes
 
-top10 <- head(VariableFeatures(UMI), 10)
+top20 <- head(VariableFeatures(UMI), 20)
+top50 <- head(VariableFeatures(UMI), 50)
+top100 <- head(VariableFeatures(UMI), 100)
 
 plot1 <- VariableFeaturePlot(UMI)
 
 jpeg(file.path(OUTPUT, "variable_genes.jpeg") , units="in", width=10, height=7, res=300)
-plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot2 <- LabelPoints(plot = plot1, points = top20, repel = TRUE)
 plot2
 dev.off()
 
@@ -192,28 +192,25 @@ dims <- as.data.frame(Elbow$data$stdev)
 #select the most variable reduction
 
 {
-  dim <- c()
+  dim <- 1
   score <- c()
   element <- 0
   for (i in dims$`Elbow$data$stdev`) {
     element <- element + 1
-    score <- cbind(i)
-    print(dims$`Elbow$data$stdev`[element+1])
-    print(i)
-    if (i-0.05*i >= dims$`Elbow$data$stdev`[element+1]) {
-      dim <- element
-    } else break
+    if (i-i*0.01 > dims$`Elbow$data$stdev`[element+1] & element < 20 | i-i*0.03 > dims$`Elbow$data$stdev`[element+2] & element < 20) {
+      dim <- dim + 1
+    } else 
+      break
   }
-  
-  
-  dim <- as.numeric(element) + 1
+  dim <- as.numeric(dim)
 }
+
 
 #########################################################################################
 
 
 jpeg(file.path(OUTPUT, "JackStrawPlot.jpeg") , units="in", width=10, height=7, res=300)
-JackStrawPlot(UMI, dims = 1:(dim+1))
+JackStrawPlot(UMI, dims = 1:(dim))
 dev.off()
 
 jpeg(file.path(OUTPUT, "PCA_heatmap.jpeg") , units="in", width=10, height=7, res=300)
@@ -237,55 +234,46 @@ dev.off()
 
 
 # find markers for every cluster compared to all remaining cells, report only the positive ones
-UMI.markers <- FindAllMarkers(UMI, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-top10 <- UMI.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
-
-
-
-##Cells cluster naming with top genes (different between cell groups)
-
-{
-  top10 <- data.frame(top10$cluster, top10$gene)
-  colnames(top10) <- c('cluster','gene')
-  
-  
-  all_unique('top10','[1:2]','cluster')
-  
-  top10$cluster <- top10$Group.1
-  top10 <- top10[,-1] 
-}
+# UMI.markers <- FindAllMarkers(UMI, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+# top10 <- UMI.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
+# 
+# 
+# 
+# ##Cells cluster naming with top genes (different between cell groups)
+# 
+# {
+#   top10 <- data.frame(top10$cluster, top10$gene)
+#   colnames(top10) <- c('cluster','gene')
+#   
+#   
+#   all_unique('top10','[1:2]','cluster')
+#   
+#   top10$cluster <- top10$Group.1
+#   top10 <- top10[,-1] 
+# }
 
 ##########################################################
-### LogFC genes select and cell nameing ###
+### Most variable genes select and cell subtypes nameing ###
 
-
+`%!in%` = Negate(`%in%`)
+genes <- markers_subclass[1,]
 exp_matrix <- as.data.frame(GetAssayData(UMI, slot = 'data'))
 colnames(exp_matrix) <- UMI@active.ident
 exp_matrix <- as.data.frame(exp_matrix)
+exp_matrix <- exp_matrix[toupper(rownames(exp_matrix)) %!in% genes[1,],]
+exp_matrix <- exp_matrix[rownames(exp_matrix) %in% top20,]
 
 
-lvl_num <- as.numeric(max(levels(UMI))) +1 
-{
-  for (lvl in 1:lvl_num) {
-    for (i in 1:ncol(exp_matrix)) {
-      rename_df <- c()
-      col <- lvl -1
-      rename_df <- exp_matrix[rownames(exp_matrix) %in% top10$gene[[lvl]],]
-      if (colnames(rename_df[i]) == as.numeric(col)) {
-        rename_df <- rename_df[order(rename_df[,i], decreasing = T),]
-        rename_df <- rownames(rename_df[1:2,])
-        rename_df <- sort(rename_df)
-        colnames(exp_matrix)[i] <- paste(rename_df[1], rename_df[2])
-      }
-    }
-  } 
-  
+num <- as.numeric(length(genes))
+index = 0
+for (i in colnames(exp_matrix)) {
+  index = index +1
+  tmp <- as.data.frame(exp_matrix[order(exp_matrix[,index], decreasing = T), ,drop =F])
+  colnames(exp_matrix)[index] <- rownames(tmp[1,])
 }
 
 cell_names <- colnames(exp_matrix)
-cell_nameing(matrix_a = exp_matrix, markers = markers)
-colnames(exp_matrix) <- paste(colnames(exp_matrix), cell_names)
-
+rm(exp_matrix)
 
 
 ############################################
@@ -295,62 +283,86 @@ average_expression <- as.data.frame(GetAssayData(UMI, slot = 'data'))
 colnames(average_expression) <- UMI@active.ident
 average_expression <- as.data.frame(average_expression)
 average_expression <- t(average_expression)
-average_expression <- aggregate(average_expression, by = list(rownames(average_expression)), FUN = mean, trim = 0.25)
+average_expression <- aggregate(average_expression, by = list(rownames(average_expression)), FUN = mean)
 rownames(average_expression) <- average_expression[,1]
 average_expression <- average_expression[,-1]
 average_expression <- t(average_expression)
 
-cluster_nameing(matrix_a = average_expression, markers = markers)
+cluster_nameing(matrix_a = average_expression, markers = markers_class)
 
-new.cluster.ids <- colnames(average_expression)
+clust_names <- colnames(average_expression)
+
+
+##########################################
+
+
+genes <- markers_subclass[1,]
+
+num <- as.numeric(length(genes))
+index = 0
+for (i in colnames(average_expression)) {
+  index = index +1
+  rename_df <- c()
+  rename_df <- average_expression[toupper(rownames(average_expression)) %in% genes[1,],]
+  rename_df <- as.data.frame(rename_df[order(rename_df[,index], decreasing = T), ,drop = F])
+  colnames(average_expression)[index] <- rownames(rename_df[1,])
+}
+
+new.cluster.ids <- paste(clust_names, colnames(average_expression))
 names(new.cluster.ids) <- levels(UMI)
 UMI <- RenameIdents(UMI, new.cluster.ids)
 
-rm(average_expression)
+
+
+exp_matrix <- as.data.frame(GetAssayData(UMI, slot = 'data'))
+colnames(exp_matrix) <- UMI@active.ident
+exp_matrix <- as.data.frame(exp_matrix)
+colnames(exp_matrix) <- paste(UMI@active.ident, cell_names)
 
 
 #PCA plot and UMAP plot with names
 
-jpeg(file.path(OUTPUT, "PCA_DimPlot.jpeg") , units="in", width=10, height=7, res=300)
+#Class
+
+jpeg(file.path(OUTPUT, "PCA_DimPlot_class.jpeg") , units="in", width=10, height=7, res=300)
 DimPlot(UMI, reduction = "pca")
 dev.off()
 
-jpeg(file.path(OUTPUT, "UMAP_with_DE_gene.jpeg") , units="in", width=10, height=7, res=300)
+jpeg(file.path(OUTPUT, "UMAP_with_DE_gene_class.jpeg") , units="in", width=10, height=7, res=300)
 DimPlot(UMI, reduction = "umap") 
 dev.off()
 
 
-#Cluster tree for show connection between groups
+#Subtypes
 
-UMI <- BuildClusterTree(object = UMI, slot = 'data', verbose = T)
+Idents(UMI) <- colnames(exp_matrix)
 
-tree_cluster <- Tool(object = UMI, slot = 'BuildClusterTree')
-
-
-jpeg(file.path(OUTPUT, "hierarchical_tree.jpeg") , units="in", width=10, height=7, res=300)
-plot(tree_cluster)
+jpeg(file.path(OUTPUT, "PCA_DimPlot_subtypes.jpeg") , units="in", width=20, height=10, res=300)
+DimPlot(UMI, reduction = "pca")
 dev.off()
+
+jpeg(file.path(OUTPUT, "UMAP_with_DE_gene_subtypes.jpeg") , units="in", width=20, height=10, res=300)
+DimPlot(UMI, reduction = "umap") 
+dev.off()
+
 
 #Create Expression Matrix
 
 #Expression matrix cells
 
-
-write.table(exp_matrix, file = file.path(OUTPUT, "expression_matrix_cells.csv"))
+write.table(exp_matrix, file = file.path(OUTPUT, "expression_matrix_cells.csv"), sep = ',')
 
 #Average expression matrix populations
 
 average_expression <- as.data.frame(exp_matrix)
-rm(exp_matrix)
 average_expression <- t(average_expression)
-average_expression <- aggregate(average_expression, by = list(rownames(average_expression)), FUN = mean, trim = 0.25)
+average_expression <- aggregate(average_expression, by = list(rownames(average_expression)), FUN = mean)
 rownames(average_expression) <- average_expression[,1]
 average_expression <- average_expression[,-1]
 average_expression <- t(average_expression)
 
 
-
-write.table(average_expression, file = file.path(OUTPUT, "average_expression_matrix_populations.csv"))
+write.table(average_expression, file = file.path(OUTPUT, "average_expression_matrix_populations.csv"), sep = ',')
 
 
 #save seurat output file
@@ -360,20 +372,15 @@ saveRDS(UMI, file = file.path(OUTPUT, "Results.rds"))
 
 #######################################################################################
 
-top_genes <- top10$gene[[1]]
-index <- 1
-for (i in 2:nrow(top10)){
-  index <- index + 1
-  top_genes <- c(top_genes, top10$gene[[index]])
-}
-
+#Cell populations pheatmaps for all genes
 
 average_expression <- as.data.frame(average_expression)
-phet_exp_matrix <- average_expression[rownames(average_expression) %in% top_genes,]
-jpeg(file.path(OUTPUT, "pheatmap_cells_populations.jpeg") , units="in", width=15, height=7, res=300)
+phet_exp_matrix <- average_expression[top50,]
+phet_exp_matrix <- drop_na(phet_exp_matrix)
+phet_exp_matrix <- scale(phet_exp_matrix)
+jpeg(file.path(OUTPUT, "pheatmap_cells_populations.jpeg") , units="in", width=15, height = 10, res=300)
 pheatmap::pheatmap(phet_exp_matrix, 
-                          clustering_method = 'complete', 
-                          cluster_rows = F)
+                   clustering_method = 'complete')
 dev.off()
 
 
