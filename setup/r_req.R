ad78b7775c9451c9b6f8b9d4f0d6d04ee0e6a3ea
jkubis96 <jakub.kubis1996@gmail.com>
2024-06-06 23:17:10 +0200

    r

--- setup/r_req.R
+++ setup/r_req.R
@@ -1,13 +1,11 @@
  
 Sys.setenv(R_INSTALL_STAGED = FALSE)
 
-install.packages('remotes', dependencies = TRUE, ask = FALSE)
-install.packages('patchwork', version = '1.1.1', dependencies = TRUE, ask = FALSE)
-# remotes::install_version("Seurat", version = "3.1.5", dependencies = TRUE, ask = FALSE)
-install.packages('viridis', version = "0.5.1", dependencies = TRUE, ask = FALSE)
-install.packages('textclean', version = "0.9.3", dependencies = TRUE, ask = FALSE)
-install.packages('ape', version = '5.4.1', dependencies = TRUE, ask = FALSE)
-install.packages('umap', dependencies = TRUE, ask = FALSE)
+install.packages('patchwork', version = '1.1.1', dependencies = FALSE, ask = FALSE)
+install.packages('viridis', version = "0.5.1", dependencies = FALSE, ask = FALSE)
+install.packages('textclean', version = "0.9.3", dependencies = FALSE, ask = FALSE)
+install.packages('ape', version = '5.4.1', dependencies = FALSE, ask = FALSE)
+install.packages('umap', dependencies = FALSE, ask = FALSE)
 
 BiocManager::install("MAST", ask = FALSE)
 
