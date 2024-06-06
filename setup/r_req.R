270c6815eab3cbbb46236917f10b6e756e6e084e
jkubis96 <jakub.kubis1996@gmail.com>
2024-06-06 19:30:28 +0200

    r

--- setup/r_req.R
+++ setup/r_req.R
@@ -3,7 +3,7 @@ Sys.setenv(R_INSTALL_STAGED = FALSE)
 
 install.packages('remotes', dependencies = TRUE, ask = FALSE)
 install.packages('patchwork', version = '1.1.1', dependencies = TRUE, ask = FALSE)
-remotes::install_version("Seurat", version = "3.1.5", dependencies = TRUE, ask = FALSE)
+# remotes::install_version("Seurat", version = "3.1.5", dependencies = TRUE, ask = FALSE)
 install.packages('viridis', version = "0.5.1", dependencies = TRUE, ask = FALSE)
 install.packages('textclean', version = "0.9.3", dependencies = TRUE, ask = FALSE)
 install.packages('ape', version = '5.4.1', dependencies = TRUE, ask = FALSE)
