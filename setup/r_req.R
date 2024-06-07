2cceacc759f38a66aac8618aff6ab65a180de2f4
jkubis96 <jakub.kubis1996@gmail.com>
2024-06-07 10:43:33 +0200

    r

--- setup/r_req.R
+++ setup/r_req.R
@@ -1,6 +1,6 @@
 Sys.setenv(R_INSTALL_STAGED = FALSE)
 
-install.packages('patchwork', version = '1.1.1')
+# install.packages('patchwork', version = '1.1.1')
 # install.packages('viridis', version = "0.5.1", dependencies = FALSE, ask = FALSE)
 install.packages('textclean', version = "0.9.3")
 # install.packages('ape', version = '5.4.1', dependencies = FALSE, ask = FALSE)
