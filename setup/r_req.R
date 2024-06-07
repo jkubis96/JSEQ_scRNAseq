4f41c3db77b02ef9d3e90704f1d7e98ac7026d05
jkubis96 <jakub.kubis1996@gmail.com>
2024-06-07 07:44:51 +0200

    r

--- setup/r_req.R
+++ setup/r_req.R
@@ -2,11 +2,10 @@
 Sys.setenv(R_INSTALL_STAGED = FALSE)
 
 install.packages('patchwork', version = '1.1.1', dependencies = FALSE, ask = FALSE)
-install.packages('viridis', version = "0.5.1", dependencies = FALSE, ask = FALSE)
+# install.packages('viridis', version = "0.5.1", dependencies = FALSE, ask = FALSE)
 install.packages('textclean', version = "0.9.3", dependencies = FALSE, ask = FALSE)
-install.packages('ape', version = '5.4.1', dependencies = FALSE, ask = FALSE)
+# install.packages('ape', version = '5.4.1', dependencies = FALSE, ask = FALSE)
 install.packages('umap', dependencies = FALSE, ask = FALSE)
-
 BiocManager::install("MAST", ask = FALSE)
 
 
