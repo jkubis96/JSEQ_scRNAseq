c013ad3eac905225800ffeda10f6e9ffeafcd8ce
jkubis96 <jakub.kubis1996@gmail.com>
2024-06-07 10:48:04 +0200

    r

--- setup/r_req.R
+++ setup/r_req.R
@@ -1,7 +1,6 @@
 Sys.setenv(R_INSTALL_STAGED = FALSE)
 
 
-Sys.setenv(R_INSTALL_STAGED = FALSE)
 BiocManager::install("MAST")
 install.packages('textclean', version = "0.9.3")
 install.packages('umap') 
